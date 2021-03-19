1c56dbea0596d248fbe2988c13ac5c5729cb9060
geekette <jaimaille@gmail.com>
2021-03-19 10:49:59 +0100

    first setting to test pycharm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("server.app:fastapp", host="127.0.0.1", port=5000, reload=True)
+    uvicorn.run("server.app:fastapp", host="95.142.160.136", port=3002, reload=True)
